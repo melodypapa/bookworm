ae81acccf8221c6efb2348e75955ab95051836b1
Melodypapa <melodypapa@outlook.com>
2021-01-22 13:21:45 +0800

    get the page from douban web page

--- src/lib/parser/douban.book.parser.ts
+++ src/lib/parser/douban.book.parser.ts
@@ -3,63 +3,34 @@ import { Book } from '../model/book.type';
 import { BookParser } from './book.parser';
 import * as cheerio from 'cheerio';
 import * as puppeteer from 'puppeteer';
+import { Console } from 'console';
 
 export class DoubanBookParser extends BookParser {
 
     public fetchBook(isbn: string): Promise<Book> {
         return new Promise(async (resolve, reject) => {
             try {
-                isbn = "14919157651";
                 const uri: string = `https://search.douban.com/book/subject_search?search_text=${isbn}&cat=1001`;
-                const browser = await puppeteer.launch();
+                const browser = await puppeteer.launch({ headless: false });
                 const page = await browser.newPage();
-                await page.goto(uri, {waitUntil: 'networkidle2'});
-                const body: string  = await page.content();
-                await browser.close();
-                /*const resp: Response = await fetch(uri);
-                const body: string = await resp.text();*/
-                console.log(body);
+                await page.goto(uri);
+                await page.screenshot({ path: 'c:\\tmp\\example.png' });
+                const body: string = await page.content();
+                
+                //console.log(body);
                 const $ = cheerio.load(body);
-                const header = $("div#wrapper h1");
+                const regex = /https:\/\/book\.douban\.com\/subject\/\d+\//;
+                const results = $('.item-root a');
+                if (results.length === 0){
+                    throw new BookWormError("Book can not be found");
+                    
+                }
+                // await page.goto($(results?[0]).attr('href'));
+                await browser.close();
                 const book: Book = {
                     name: "",
                     uri: uri,
                 };
-
-                /*const details = {};
-                for (let idx = 0; idx < detailKeyTags.length; idx++) {
-                    const key = $(detailKeyTags[idx]).text().replace(/[\s:]+/, "").toLowerCase();
-                    const value = $(detailValueTags[idx]).text();
-                    switch (key) {
-                        case "publisher":
-                            const match = value.match(/([\w\s']+);?([\w\s]+)?\s?\((\w+)\s+(\d{1,2}),\s+(\d{4})\)/);
-                            if (match) {
-                                book.publisher = this.stripBlank(match[1]);
-                                book.edition = this.stripBlank(match[2]);
-                                book.publishDate = {
-                                    year: parseInt(match[5]),
-                                    month: this.convertMonth(match[3]),
-                                    day: parseInt(match[4]),
-                                };
-                            }
-                            else {
-                                book.publisher = this.stripBlank(value);
-                            }
-                            break;
-                        case "language":
-                            book.language = value;
-                            break;
-                        case "isbn-10":
-                            book.isbn10 = value;
-                            break;
-                        case "isbn-13":
-                            book.isbn13 = value;
-                            break;
-                        case "paperback":
-                            book.paperback = value;
-                            break;
-                    }
-                }*/
                 resolve(book);
             }
             catch (err) {
@@ -67,5 +38,5 @@ export class DoubanBookParser extends BookParser {
             }
         });
     }
-    
+
 }
