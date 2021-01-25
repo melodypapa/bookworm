704b913428a725f1f2fadc88fcf43695cd9e9074
Melodypapa <melodypapa@outlook.com>
2021-01-25 13:18:58 +0800

    update the douban parser

--- src/lib/parser/douban.book.parser.ts
+++ src/lib/parser/douban.book.parser.ts
@@ -7,10 +7,47 @@ import { Console } from 'console';
 
 export class DoubanBookParser extends BookParser {
 
-    public fetchBook(isbn: string): Promise<Book> {
+    private searchBook(isbn: string): Promise<string | undefined> {
         return new Promise(async (resolve, reject) => {
             try {
                 const uri: string = `https://search.douban.com/book/subject_search?search_text=${isbn}&cat=1001`;
+                const browser = await puppeteer.launch({ headless: true });
+                const page = await browser.newPage();
+                await page.goto(uri);
+                await page.screenshot({ path: 'c:\\tmp\\example.png' });
+                const body: string = await page.content();
+                
+                //console.log(body);
+                const $ = cheerio.load(body);
+                
+                const regexLink = /https:\/\/book\.douban\.com\/subject\/\d+\//;
+                const results = $('.item-root a');
+                if (results.length === 0){
+                    throw new BookWormError("Book can not be found");
+                }
+                await browser.close();
+                for (let idx = 0; idx < results.length; idx ++) {
+                    if ($(results[idx]).attr('href')?.match(regexLink)){
+                        resolve($(results[idx]).attr('href'));
+                        return;
+                    }
+                }
+                throw new BookWormError("Book can not be found");
+            }
+            catch (err) {
+                reject(err);
+            }
+        });
+    }
+
+    public fetchBook(isbn: string): Promise<Book> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const uri = await this.searchBook(isbn);
+                if (uri === undefined){
+                    throw new BookWormError("Book can not be found");
+                }
+
                 const browser = await puppeteer.launch({ headless: false });
                 const page = await browser.newPage();
                 await page.goto(uri);
@@ -23,7 +60,6 @@ export class DoubanBookParser extends BookParser {
                 const results = $('.item-root a');
                 if (results.length === 0){
                     throw new BookWormError("Book can not be found");
-                    
                 }
                 // await page.goto($(results?[0]).attr('href'));
                 await browser.close();
